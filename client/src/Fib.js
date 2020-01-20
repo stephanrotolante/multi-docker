140067eddfb6cb914f894bbd070d6e6a0d460063
stephanrotolante <stephanrotolante@gmail.com>
2020-01-19 21:16:56 -0500

    array checker

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -14,20 +14,24 @@ class Fib extends Component {
     }
 
     componentDidMount() {
-        // this.fetchValues();
-        // this.fetchIndexes();
+        this.fetchValues();
+        this.fetchIndexes();
     }
 
     async fetchValues() {
         const values = await axios.get('/api/values/current');
+        
         this.setState({values: values.data});
     }
 
     async fetchIndexes() {
         const seenIndexes = await axios.get('/api/values/all');
-        this.setState({
-            seenIndexes:seenIndexes.data
-        })
+
+        if(Array.isArray(seenIndexes.data)) {
+            this.setState({
+                seenIndexes:seenIndexes.data
+            })
+        }
     }
 
     handleSubmit = async (event) => {
